f27eda3e147625cf2febfe993051b7db0dfdf88f
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-03-02 16:28:10 +0100

    finalCommit

--- src/2.ts
+++ src/2.ts
@@ -16,15 +16,7 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
+  constructor(public name: string, private department: string, protected salary: number) {}
 
   getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
@@ -33,7 +25,15 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(nameManager: string, salaryManager: number) {
+    super(nameManager, "Management", salaryManager);
+    this.salary += 10000;
+  }
 }
 
+const manager = new Manager('Taylor Swift', 20000);
+console.log(manager.getEmployeeDetails());
+
+//Name: Taylor Swift, Department: Management, Salary: 30000
 
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,68 @@
+//У цьому завдання вам належить реалізувати сценарій життя, де людина, ключ і будинок взаємодіють один з одним.
+
+class Key {
+    private readonly signature: number;
+
+    constructor() {
+    this.signature = Math.random();
+  }
+
+    getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Person entered the house.');
+    } else {
+      console.log('The door is closed. Cannot enter.');
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+  this.door = key.getSignature() === this.key.getSignature();
+  console.log(this.door ? 'Door opened successfully.' : 'Invalid key. Door remains closed.');
+}
+}
+    
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
 
+//Door opened successfully
+//Person entered the house.
 
-export {};
+export { };
